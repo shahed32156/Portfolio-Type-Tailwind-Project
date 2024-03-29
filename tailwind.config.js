1953499b350b57a939aad423e596000257a4e4a2
shahed32156 <shahed32156@gmail.com>
2024-03-29 21:48:01 +0600

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./css/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+    colors: {
+
+     "primary": "#FF9D00",
+     "secondary": "#183656"
+
+    }
+
+
+    },
+  },
+  plugins: [],
+}
